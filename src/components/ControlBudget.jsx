38b19036f7a46e9253ad6cf03b6b6bef8051f65a
JordiTrassi <j.trassi@telefonica.net>
2022-10-07 23:41:57 +0200

    Added formatearCantidad function in ControlBudget component

--- src/components/ControlBudget.jsx
+++ src/components/ControlBudget.jsx
@@ -1,19 +1,34 @@
 
 
 export const ControlBudget = ({presupuesto}) => {
-  return (
-      <div className="contenedor-presupuesto contenedor sombra dos-columnas">
-        <div>
-            {/* grafica */}
-        </div>
-          <div className="contenido-presupuesto">
-              <p>
-                  <span>Presupuesto: </span> {presupuesto} € 
-              </p>
 
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('de-DE', {
+            style: 'currency',
+            currency: 'EUR'
+        })
+    };
+
+
+    return (
+        <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+            <div>
+                {/* grafica */}
+            </div>
+            <div className="contenido-presupuesto">
+                <p>
+                    <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+                </p>
+                <p>
+                    <span>Disponible: </span> {formatearCantidad(0)}
+                </p>
+                <p>
+                    <span>Gastado: </span> {formatearCantidad(0)}
+                </p>
+
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
 
