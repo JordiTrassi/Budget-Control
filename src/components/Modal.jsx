4e007397429fafe40afd72928a5652cbfc065c87
JordiTrassi <j.trassi@telefonica.net>
2022-10-08 22:22:28 +0200

    Added Modal component structure

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -22,6 +22,43 @@ export const Modal = ({setModal, animarModal, setAnimarModal}) => {
 
             <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
                 <legend>Nuevo Gasto:</legend>
+
+                <div className="campo">
+                    <label htmlFor="nombre">Nombre Gasto:</label>
+                    <input
+                        id="nombre"
+                        type="text"
+                        placeholder="Añade el Nombre del Gasto"
+                    />
+                </div>
+
+                <div className="campo">
+                    <label htmlFor="cantidad">Cantidad:</label>
+                    <input
+                        id="cantidad"
+                        type="number"
+                        placeholder="Añade la Cantidad del Gasto"
+                    />
+                </div>
+
+                  <div className="campo">
+                    <label htmlFor="categoria">Categoría:</label>
+                    <select id="categoria">
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos Varios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                    </select>
+                </div>
+
+                <input
+                    type="submit"
+                    value="Añadir Gasto"
+                />
             </form>
         </div>
     );
