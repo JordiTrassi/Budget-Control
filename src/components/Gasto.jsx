59b54f43877eaff872fff2b752630053d561d09f
JordiTrassi <j.trassi@telefonica.net>
2022-10-09 17:26:35 +0200

    Installed prop-types

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,3 +1,12 @@
+import { 
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list';
+// import 'react-swipeable-list/dist/styles.css';
+
 import { formatDate } from '../helpers/formatDate';
 
 import IconoAhorro from '../img/icono_ahorro.svg';
@@ -23,23 +32,27 @@ export const Gasto = ({ gasto }) => {
     const { categoria, nombre, cantidad, id, fecha } = gasto;
 
     return (
-        <div className="gasto sombra">
-            <div className="contenido-gasto">
-                <img
-                    src={diccionarioIconos[categoria]}
-                    alt="Icono-Gasto"
-                />
-                <div className="descripcion-gasto">
-                    <p className="categoria">{categoria}</p>
-                    <p className="nombre-gasto">{nombre}</p>
-                    <p className="fecha-gasto">
-                        Agregado: {''}
-                        <span>{formatDate(fecha)}</span>
-                    </p>
+        <SwipeableList>
+            <SwipeableListItem>
+                <div className="gasto sombra">
+                    <div className="contenido-gasto">
+                        <img
+                            src={diccionarioIconos[categoria]}
+                            alt="Icono-Gasto"
+                            />
+                        <div className="descripcion-gasto">
+                            <p className="categoria">{categoria}</p>
+                            <p className="nombre-gasto">{nombre}</p>
+                            <p className="fecha-gasto">
+                                Agregado: {''}
+                                <span>{formatDate(fecha)}</span>
+                            </p>
+                        </div>
+                    </div>
+                    <p className='cantidad-gasto'>{cantidad} €</p>
                 </div>
-            </div>
-            <p className='cantidad-gasto'>{cantidad} €</p>
-        </div>
+            </SwipeableListItem>
+        </SwipeableList>
     );
 }
 
